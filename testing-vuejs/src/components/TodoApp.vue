fa9a88ef7be484e02cbb43b3d995db4802a73a25
gabrieldev-7cd <gabriel.alexandre.dev@gmail.com>
2022-01-07 16:12:27 -0300

    Checkbox done todo function and test created and component mount beforeEach added in test files

--- testing-vuejs/src/components/TodoApp.vue
+++ testing-vuejs/src/components/TodoApp.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-        <div v-for="todo in todos" :key="todo.id" data-test="todo">
-            {{todo}}
+        <div 
+            v-for="todo in todos"
+            :key="todo.id"
+            data-test="todo"
+            :class="[ todo.completed ? 'completed' : '' ]"
+        >
+            {{ todo.text }}
+            <input 
+                type="checkbox"
+                v-model="todo.completed"
+                data-test="todo-checkbox"
+            />
         </div>
 
         <form data-test="form" @submit.prevent="createTodo" >
@@ -43,5 +53,10 @@ export default {
 </script>
 
 <style>
+
+.completed
+{
+    color: green;
+}
 
 </style>
--- testing-vuejs/tests/unit/todoapp.spec.js
+++ testing-vuejs/tests/unit/todoapp.spec.js
@@ -2,22 +2,33 @@ import { mount } from '@vue/test-utils'
 import TodoApp from '@/components/TodoApp.vue'
 
 describe("TodoApp.vue", () => {
-    it("should render todo text", () => {
 
-        const wrapper = mount(TodoApp);
+    let wrapper;
+
+    beforeEach( () => {
+        wrapper = mount(TodoApp);
+    })
+
+    it("should render todo text", () => {
+        // const wrapper = mount(TodoApp);
         const todo = wrapper.get('[data-test="todo"]');
-        // expect(todo.text()).toBe("Learn Vue Testing");
         expect(todo.text()).toMatch("Learn Vue Testing");
+        expect( typeof todo.text()).toBe('string');
     })
 
     it("should add new todo", async () => {
-        const wrapper = mount(TodoApp);
+        // const wrapper = mount(TodoApp);
 
         expect(wrapper.findAll('[data-test="todo"')).toHaveLength(1);
-
         await wrapper.get('[data-test="new-todo"]').setValue('New Todo');
         await wrapper.get('[data-test="form"]').trigger('submit');
-
         expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2);
+    })
+
+    it("should be able to complete todo", async () => {
+        // const wrapper = mount(TodoApp);
+   
+        await wrapper.get(' [data-test="todo-checkbox" ').setValue(true);
+        expect(wrapper.get(' [data-test="todo" ').classes()).toContain('completed');
     })
 })
